f61b9296ddbb44ec8bd4482dd7fba52838727261
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2024-08-21 10:21:57 +0500

    Create Q3.py

---
+++ LABS/01/Q3.py
@@ -0,0 +1,18 @@
+x=int(input("enter your score : "))
+
+if(x>=80):
+    if(x>85):
+        print("CSsecA")
+    elif(x<=85):
+        print("CSsecB")
+elif(x>=70):
+    if(x>75):
+        print("AIsecA")
+    if(x<=75):
+        print("AIsecB")
+
+elif(x>=60):
+    if(x>65):
+        print("DSsecA")
+    if(x<=65):
+        print("DSsecB")
