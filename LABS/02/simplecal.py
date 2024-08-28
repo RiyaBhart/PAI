aa6e4cda54d6282614e991a71dec380364ee25e2
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2024-08-28 09:18:43 +0500

    Create simplecal.py

---
+++ LABS/02/simplecal.py
@@ -0,0 +1,9 @@
+def calculator():
+    x=int(input("enter an integer: "))
+    y=int(input("enter another integer: "))
+    print("result after addition : ",x+y)
+    print("result after subtraction : ",x-y)
+    print("result after multiplication : ",x*y)
+    print("result after divison : ",x/y)
+
+calculator()
