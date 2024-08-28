fed9b1ccf492a3868af81756a0784ff906d2946f
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2024-08-28 09:43:22 +0500

    Update practicetask2.0.py

--- LABS/02/practicetask2.0.py
+++ LABS/02/practicetask2.0.py
@@ -1,15 +1,14 @@
 def userfeedback():
     name = input("Your good name: ")
-    print(f"Hello! {name}")
+    print("Hello!",name)
     print("I hope you are fine, let me know how I can help you!")
-
+    
     response = input("Do you need help? (yes/no): ").strip().lower()
-
+    
     if response == 'yes':
         problem = input("Share your problem with us: ")
         print("Thanks for your feedback, we will resolve your problem.")
     else:
-        print(f"{'Okay! Good to see you, stay connected'.center(44)}")
-
+        print('Okay! Good to see you, stay connected'.center(40))
 
 userfeedback()
