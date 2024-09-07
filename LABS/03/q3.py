acba27cea8aa44458b6b5ff48207343a1785f92a
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2024-09-07 19:34:17 +0500

    Create q3.py

---
+++ LABS/03/q3.py
@@ -0,0 +1,15 @@
+try:
+    list1 = input("Enter the first list elements : ").split()
+    list2 = input("Enter the second list elements : ").split()
+
+    if len(list1) != len(list2):
+        print("Error: The lists must have the same number of elements.")
+    else:
+        dictionary = {}
+        for i in range(len(list1)):
+            dictionary[list1[i]] = list2[i]
+        print(dictionary)
+        with open("q3.txt","w") as file:
+            file.write(str(dictionary))
+except Exception as e:
+    print("an error occured ",e)
