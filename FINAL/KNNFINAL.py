98b1b369e6d9b57e1981ef035290e485741d2528
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2024-12-03 19:17:16 +0500

    Update KNNFINAL.py

--- FINAL/KNNFINAL.py
+++ FINAL/KNNFINAL.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.metrics import confusion_matrix,precision_score,f1_score,recall_score, accuracy_score, classification_report
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
@@ -43,3 +43,32 @@ print("\nClassification Report:\n", classification_report(Y_test, y_pred))
 cv_scores = cross_val_score(knn, X, Y, cv=4)
 print("\nFold Cross Validation Scores:", cv_scores)
 print("Mean CV Accuracy:", np.mean(cv_scores))
+
+precision = precision_score(Y_test,y_pred,average='macro')
+print(f"Precision: {precision:.2f}")
+
+recall = recall_score(Y_test, Y_test, average='macro')
+print(f"Recall: {recall:.2f}")
+
+f1 = f1_score(Y_test, Y_test,average='macro')
+print(f"F1 Score: {f1:.2f}")
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+lr = LogisticRegression()
+lr.fit(X_train, Y_train)
+y_pred_lr = lr.predict(X_test)
+print("Accuracy:", accuracy_score(Y_test, y_pred_lr))
+print("Confusion Matrix:\n", confusion_matrix(Y_test, y_pred_lr))
+print("Classification Report:\n", classification_report(Y_test, y_pred_lr))
+
+svm = SVC()
+svm.fit(X_train, Y_train)
+y_pred_svm = svm.predict(X_test)
+print("Accuracy:", accuracy_score(Y_test, y_pred_svm))
+print("Confusion Matrix:\n", confusion_matrix(Y_test, y_pred_svm))
+print("Classification Report:\n", classification_report(Y_test, y_pred_svm))
+
